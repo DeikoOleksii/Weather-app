8e8ffe456fed87ff2ae4f8517ddb6fa9818b49ac
Oleksii_Deiko <deyko@dlit.dp.ua>
2022-01-05 17:21:03 +0200

    add error handling

--- script.js
+++ script.js
@@ -3,37 +3,40 @@ const apikey = "7c89dfc6b8c54ab5daa516325e286b4b"
 const main = document.getElementById("main")
 const form = document.getElementById("form")
 const search = document.getElementById("search")
+const container = document.createElement("div")
+main.appendChild(container)
+container.classList.add("container")
 
 const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
-
 
 const convertToCelcium = (degree) => {
   return (degree - 273).toFixed(2)
 };
 
-const container = document.createElement("div")
-
 const showWeather = (weather) => {
 
-    container.classList.add("container")
     const temp = convertToCelcium(weather.main.temp)
-
     container.innerHTML = `
         <h2><img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" /></h2>
         <small>${weather.weather[0].main}</small>
     `
+}
 
-
-    main.appendChild(container)
+const showError = (error) => {
+    container.innerHTML = `<small>Cannot find such city</small>`
 }
 
 const getWeather = async (city) => {
-    const response = await fetch(url(city), { origin: "cors" })
-    const responseData = await response.json()
-
-    console.log(responseData, convertToCelcium(responseData.main.temp))
-
-    showWeather(responseData)
+    
+    try{
+        const response = await fetch(url(city), { origin: "cors" })
+        const responseData = await response.json()
+        showWeather(responseData)
+    }
+    catch(e)
+    {    
+        showError(e)
+    }
 
 }
 
